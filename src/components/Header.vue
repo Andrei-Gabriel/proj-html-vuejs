11862f9e88c1ba01c4db15f4cb42b2dee4f34f70
Andrei-Gabriel <andrescu.gabriel.andrei@gmail.com>
2022-01-12 19:28:34 +0100

    proprietà header-top quando si scorre la pagina

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
     <header>
-        <div class="header-content">
-            <div class="header-top">
+        <div class="header-top">
+            <div class="header-top-content">
                 <img src="../assets/img/avadabarbers-logo-x1.png" alt="Logo Avada Baebers">
                 <div class="header-icons">
                     <i class="fas fa-shopping-cart"></i>
@@ -9,6 +9,7 @@
                 </div>
             </div>
         </div>
+        lorem*100
     </header>
 </template>
 
@@ -21,14 +22,23 @@
 <style scoped lang="scss">
     header {
         background-image: url("../assets/img/avadabarbers-homepage-hero-bg.jpg");
-        .header-content {
-            max-width: 1100px;
-            margin: 0 auto;
-            .header-top {
-                height: 100px;
+        height: 500px;
+        // Quando scorre height: 100px, fixed, top, left: 0, width: 100%. Non serve margin-top
+        .header-top {
+            // background-color: #000;
+            // // Non ho messo "sticky perché non scorreva oltre l'heaeder"
+            // position: fixed;
+            // width: 100%;
+            // top: 0;
+            // left: 0;
+            // z-index: 1;
+            .header-top-content {
+                height: 120px;
                 display: flex;
                 justify-content: space-between;
                 align-items: center;
+                max-width: 1100px;
+                margin: 0 auto;
                 .header-icons {
                     width: 70px;
                     display: flex;
