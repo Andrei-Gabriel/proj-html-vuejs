cd6e888d0a1a2ef482851a588a58a55213caaa3e
Andrei-Gabriel <andrescu.gabriel.andrei@gmail.com>
2022-01-12 18:37:32 +0100

    Init e creati macro-componenti

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <Header/>
+    <Main/>
+    <Footer/>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue'
+import Main from './components/Main.vue'
+import Footer from './components/Footer.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Main,
+    Footer,
+  }
+}
+</script>
+
+<style lang="scss">
+  * {
+    margin: 0;
+    padding: 0;
+    font-size: 100%;
+    box-sizing: border-box;
+  }
+</style>
