e5a4c6417879a696b6f7154f9880ca713bacd318
Andrei-Gabriel <andrescu.gabriel.andrei@gmail.com>
2022-01-12 19:13:15 +0100

    Fine header-top

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,14 @@
 <template>
     <header>
-
+        <div class="header-content">
+            <div class="header-top">
+                <img src="../assets/img/avadabarbers-logo-x1.png" alt="Logo Avada Baebers">
+                <div class="header-icons">
+                    <i class="fas fa-shopping-cart"></i>
+                    <i class="fas fa-bars"></i>
+                </div>
+            </div>
+        </div>
     </header>
 </template>
 
@@ -11,5 +19,33 @@
 </script>
 
 <style scoped lang="scss">
-
+    header {
+        background-image: url("../assets/img/avadabarbers-homepage-hero-bg.jpg");
+        .header-content {
+            max-width: 1100px;
+            margin: 0 auto;
+            .header-top {
+                height: 100px;
+                display: flex;
+                justify-content: space-between;
+                align-items: center;
+                .header-icons {
+                    width: 70px;
+                    display: flex;
+                    justify-content: space-between;
+                    i {
+                        font-size: 24px;
+                        color: #fff;
+                        cursor: pointer;
+                    }
+                    .fa-shopping-cart {
+                        transition: color 0.5s;
+                    }
+                    .fa-shopping-cart:hover {
+                        color: #ae8d67;
+                    }
+                }
+            }
+        }
+    }
 </style>
